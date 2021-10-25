d588946969b15bd221465b180f2f6757cf2bb0bc
joshdiazv <89095568+joshdiazv@users.noreply.github.com>
2021-10-25 07:30:00 -0500

    Add files via upload

---
+++ Ecommerce.Dto/CustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Dto
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string NumberId { get; set; }
+    }
+}
---
+++ Ecommerce.Dto/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string UrlImage { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
